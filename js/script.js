240cba79e4d178bc2c112902bf8516b8f78dd4ef
dubaiareaa29 <96204557+dubaiareaa29@users.noreply.github.com>
2022-02-16 00:29:09 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,25 @@
+let currentData = document.querySelector(".currentData");
+let now = new Date();
+
+now.setHours(now.getHours() - 2);
+
+function formatAMPM(date) {
+	var hours = date.getHours();
+	var minutes = date.getMinutes();
+	minutes = minutes < 10 ? "0" + minutes : minutes;
+	var strTime = hours + ":" + minutes;
+	return strTime;
+}
+
+// script for data-end
+
+let today = new Date();
+let dd = String(today.getDate()).padStart(2, '0');
+let mm = String(today.getMonth() + 1).padStart(2, '0');		//January is 0!
+let yyyy = today.getFullYear();
+
+today = dd + '/' + mm + '/' + yyyy;
+
+let rand = Math.floor(Math.random() * (15 - 5) + 5);
+$('.data-end-place').text(rand)
+$('.data-end-scr').text(today)
